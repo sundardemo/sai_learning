2ae63a38b9d37972cafab0c2404c2e92896c649c
Sundaravel M <appmaking@Sundaravels-Mac-mini.local>
2022-12-17 20:51:38 +0530

    02_image_example

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,11 @@ main() {
       // backgroundColor: Colors.brown,
       //  backgroundColor: Colors.brown[200],
       //   backgroundColor: Color.fromARGB(255, 167, 64, 27),
-      body: Text(
-        "Hello",
-        style: TextStyle(
-          color: Colors.orange,
-          fontSize: 32,
+      body: Center(
+        child: Image.network(
+          "https://fujifilm-x.com/wp-content/uploads/2021/01/gfx100s_sample_04_thum-1.jpg",
+          height: 100,
+          width: 100,
         ),
       ),
     ),
