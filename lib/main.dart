8b667b23c77aa2dfe19de0f0f7fe16f315c78a3a
Sundaravel M <appmaking@Sundaravels-Mac-mini.local>
2022-12-17 21:14:31 +0530

    04_column_example

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,32 @@
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Color(0xff4c8f61),
-      // backgroundColor: Colors.brown,
-      //  backgroundColor: Colors.brown[200],
-      //   backgroundColor: Color.fromARGB(255, 167, 64, 27),
-      body: Container(
-        margin: EdgeInsets.all(20),
-        padding: EdgeInsets.only(top: 20, left: 50, bottom: 4, right: 8),
-        color: Colors.indigo,
-        height: 100,
-        width: 100,
-        child: Center(
-          child: Text("Hii"),
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color(0xff4c8f61),
+        // backgroundColor: Colors.brown,
+        //  backgroundColor: Colors.brown[200],
+        //   backgroundColor: Color.fromARGB(255, 167, 64, 27),
+        body: Column(
+          children: [
+            Text("asdasd"),
+            Image.network(
+              "https://fujifilm-x.com/wp-content/uploads/2021/01/gfx100s_sample_04_thum-1.jpg",
+              height: 200,
+            ),
+            Container(
+              color: Colors.red,
+              height: 50,
+            ),
+            Icon(
+              Icons.join_inner,
+              size: 40,
+              color: Colors.cyan,
+            )
+          ],
         ),
       ),
     ),
-  ));
+  );
 }
