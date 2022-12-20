82a7c2c8ac82befd596b49ce52c2da5d142a2a48
Sundaravel M <appmaking@Sundaravels-Mac-mini.local>
2022-12-20 22:36:42 +0530

    06_row_example

--- lib/main.dart
+++ lib/main.dart
@@ -4,71 +4,34 @@ main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        //backgroundColor: Color(0xff4c8f61),
-        // backgroundColor: Colors.brown,
-        //  backgroundColor: Colors.brown[200],
-        //   backgroundColor: Color.fromARGB(255, 167, 64, 27),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
+          body: Container(
+        color: Colors.red,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Text("asdasd"),
-            GestureDetector(
-              onDoubleTap: () {
-                print("onDoubleTap");
-              },
-              onTap: () {
-                print("onTap");
-              },
-              child: Image.network(
-                "https://fujifilm-x.com/wp-content/uploads/2021/01/gfx100s_sample_04_thum-1.jpg",
-                height: 200,
-              ),
-            ),
             Image.asset(
-              'assets/images/logo.png',
+              "assets/images/logo.png",
               height: 100,
               width: 100,
             ),
-            SizedBox(height: 8),
-            SizedBox(
-              width: double.infinity,
-              height: 60,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text(
-                  "Click Me",
-                  style: TextStyle(color: Colors.yellow),
-                ),
-                style: ElevatedButton.styleFrom(
-                  elevation: 0,
-                  backgroundColor: Colors.red,
-                  shadowColor: Colors.amber,
-                ),
-              ),
+            Image.asset(
+              "assets/images/logo.png",
+              height: 100,
+              width: 100,
             ),
-            SizedBox(height: 8),
-            OutlinedButton(onPressed: () {}, child: Text("Click Me")),
-            SizedBox(height: 8),
-            TextButton(onPressed: () {}, child: Text("Click Me")),
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.star),
-              focusColor: Colors.red,
-              hoverColor: Colors.blue,
+            Image.asset(
+              "assets/images/logo.png",
+              height: 100,
+              width: 100,
             ),
-            Container(
-              color: Colors.red,
-              height: 50,
-            ),
-            Icon(
-              Icons.join_inner,
-              size: 40,
-              color: Colors.cyan,
+            Image.asset(
+              "assets/images/logo.png",
+              height: 100,
+              width: 100,
             )
           ],
         ),
-      ),
+      )),
     ),
   );
 }
