f2443cdfcf94b8272294d51a581704fc7704349e
Sundaravel M <appmaking@Sundaravels-Mac-mini.local>
2022-12-20 22:46:56 +0530

    07_row_column_example

--- lib/main.dart
+++ lib/main.dart
@@ -4,34 +4,60 @@ main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-          body: Container(
-        color: Colors.red,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Image.asset(
-              "assets/images/logo.png",
-              height: 100,
-              width: 100,
+        backgroundColor: Colors.grey[100],
+        body: Center(
+          child: Container(
+            color: Colors.white,
+            height: 400,
+            width: 300,
+            child: Column(
+              children: [
+                Container(
+                  height: 300,
+                  width: double.infinity,
+                  child: Image.asset(
+                    "assets/images/logo.png",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Umbber la"),
+                            Text("price"),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Row(children: [
+                          Text("4.5"),
+                          Icon(Icons.star),
+                        ]),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(onPressed: () {}, child: Text("Buy Now")),
+                      ElevatedButton(
+                          onPressed: () {}, child: Text("Add to Cart"))
+                    ],
+                  ),
+                ),
+              ],
             ),
-            Image.asset(
-              "assets/images/logo.png",
-              height: 100,
-              width: 100,
-            ),
-            Image.asset(
-              "assets/images/logo.png",
-              height: 100,
-              width: 100,
-            ),
-            Image.asset(
-              "assets/images/logo.png",
-              height: 100,
-              width: 100,
-            )
-          ],
+          ),
         ),
-      )),
+      ),
     ),
   );
 }
