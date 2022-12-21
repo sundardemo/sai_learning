46a2f71265d6f6d698f19cfebdd6dccc29ab030b
Sundaravel M <appmaking@Sundaravels-Mac-mini.local>
2022-12-21 22:05:19 +0530

    10_list_view_example

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,148 @@
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(MaterialApp(
-    home: SafeArea(
-      child: Scaffold(
-        backgroundColor: Colors.grey[100],
-        body: Container(
-          child: Stack(children: [
-            Container(
-              color: Colors.red,
-            ),
-            Positioned(
-              top: 30,
-              left: 60,
-              child: Image.asset(
-                'assets/images/logo.png',
-                height: 200,
+  runApp(
+    MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.grey[100],
+          body: Column(
+            children: [
+              Container(
+                height: 50,
+                child: ListView(scrollDirection: Axis.horizontal, children: [
+                  Container(
+                    color: Colors.grey[200],
+                    padding: EdgeInsets.all(4.0),
+                    margin: EdgeInsets.all(4.0),
+                    child: Text("Product"),
+                  ),
+                  Container(
+                    color: Colors.grey[200],
+                    padding: EdgeInsets.all(4.0),
+                    margin: EdgeInsets.all(4.0),
+                    child: Text("Product"),
+                  ),
+                  Container(
+                    color: Colors.grey[200],
+                    padding: EdgeInsets.all(4.0),
+                    margin: EdgeInsets.all(4.0),
+                    child: Text("Product"),
+                  ),
+                  Container(
+                    color: Colors.grey[200],
+                    padding: EdgeInsets.all(4.0),
+                    margin: EdgeInsets.all(4.0),
+                    child: Text("Product"),
+                  ),
+                  Container(
+                    color: Colors.grey[200],
+                    padding: EdgeInsets.all(4.0),
+                    margin: EdgeInsets.all(4.0),
+                    child: Text("Product"),
+                  ),
+                  Container(
+                    color: Colors.grey[200],
+                    padding: EdgeInsets.all(4.0),
+                    margin: EdgeInsets.all(4.0),
+                    child: Text("Product"),
+                  ),
+                  Container(
+                    color: Colors.grey[200],
+                    padding: EdgeInsets.all(4.0),
+                    margin: EdgeInsets.all(4.0),
+                    child: Text("Product"),
+                  ),
+                  Container(
+                    color: Colors.grey[200],
+                    padding: EdgeInsets.all(4.0),
+                    margin: EdgeInsets.all(4.0),
+                    child: Text("Product"),
+                  ),
+                  Container(
+                    color: Colors.grey[200],
+                    padding: EdgeInsets.all(4.0),
+                    margin: EdgeInsets.all(4.0),
+                    child: Text("Product"),
+                  ),
+                ]),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text("Hello"),
-            ),
-            Align(alignment: Alignment.bottomCenter, child: Icon(Icons.abc))
-          ]),
+              Expanded(
+                child: ListView(
+                  children: [
+                    ListTile(
+                      title: Text("iPhone"),
+                      subtitle: Text("Rs. 500"),
+                      leading: CircleAvatar(backgroundColor: Colors.green),
+                      trailing: Icon(Icons.fork_left),
+                    ),
+                    ListTile(
+                      title: Text("iPhone"),
+                      subtitle: Text("Rs. 500"),
+                      leading: CircleAvatar(backgroundColor: Colors.green),
+                      trailing: Icon(Icons.fork_left),
+                    ),
+                    ListTile(
+                      title: Text("iPhone"),
+                      subtitle: Text("Rs. 500"),
+                      leading: CircleAvatar(backgroundColor: Colors.green),
+                      trailing: Icon(Icons.fork_left),
+                    ),
+                    ListTile(
+                      title: Text("iPhone"),
+                      subtitle: Text("Rs. 500"),
+                      leading: CircleAvatar(backgroundColor: Colors.green),
+                      trailing: Icon(Icons.fork_left),
+                    ),
+                    ListTile(
+                      title: Text("iPhone"),
+                      subtitle: Text("Rs. 500"),
+                      leading: CircleAvatar(backgroundColor: Colors.green),
+                      trailing: Icon(Icons.fork_left),
+                    ),
+                    ListTile(
+                      title: Text("iPhone"),
+                      subtitle: Text("Rs. 500"),
+                      leading: CircleAvatar(backgroundColor: Colors.green),
+                      trailing: Icon(Icons.fork_left),
+                    ),
+                    ListTile(
+                      title: Text("iPhone"),
+                      subtitle: Text("Rs. 500"),
+                      leading: CircleAvatar(backgroundColor: Colors.green),
+                      trailing: Icon(Icons.fork_left),
+                    ),
+                    ListTile(
+                      title: Text("iPhone"),
+                      subtitle: Text("Rs. 500"),
+                      leading: CircleAvatar(backgroundColor: Colors.green),
+                      trailing: Icon(Icons.fork_left),
+                    ),
+                    ListTile(
+                      title: Text("iPhone"),
+                      subtitle: Text("Rs. 500"),
+                      leading: CircleAvatar(backgroundColor: Colors.green),
+                      trailing: Icon(Icons.fork_left),
+                    ),
+                    ListTile(
+                      title: Text("iPhone"),
+                      subtitle: Text("Rs. 500"),
+                      leading: CircleAvatar(backgroundColor: Colors.green),
+                      trailing: Icon(Icons.fork_left),
+                    ),
+                    ListTile(
+                      title: Text("iPhone"),
+                      subtitle: Text("Rs. 500"),
+                      leading: CircleAvatar(backgroundColor: Colors.green),
+                      trailing: Icon(Icons.fork_left),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     ),
-  ));
+  );
 }
