c806fe8bc596e76ead15c5b019cc624462a81093
Sundaravel M <appmaking@Sundaravels-Mac-mini.local>
2022-12-21 21:54:48 +0530

    09_stack_example

--- lib/main.dart
+++ lib/main.dart
@@ -5,60 +5,25 @@ main() {
     home: SafeArea(
       child: Scaffold(
         backgroundColor: Colors.grey[100],
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              // Expanded(
-              //   child: Container(
-              //     color: Colors.red,
-              //   ),
-              // ),
-              Text("sdfdsf"),
-              Container(
+        body: Container(
+          child: Stack(children: [
+            Container(
+              color: Colors.red,
+            ),
+            Positioned(
+              top: 30,
+              left: 60,
+              child: Image.asset(
+                'assets/images/logo.png',
                 height: 200,
-                child: Container(
-                  height: double.infinity,
-                  width: double.infinity,
-                  child: Image.asset(
-                    "assets/images/logo.png",
-                    fit: BoxFit.cover,
-                  ),
-                ),
               ),
-              Container(
-                padding: EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("Umbber la"),
-                          Text("price"),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      child: Row(children: [
-                        Text("4.5"),
-                        Icon(Icons.star),
-                      ]),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    ElevatedButton(onPressed: () {}, child: Text("Buy Now")),
-                    ElevatedButton(onPressed: () {}, child: Text("Add to Cart"))
-                  ],
-                ),
-              ),
-            ],
-          ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("Hello"),
+            ),
+            Align(alignment: Alignment.bottomCenter, child: Icon(Icons.abc))
+          ]),
         ),
       ),
     ),
