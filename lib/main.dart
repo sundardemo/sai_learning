ba2abce0521e489fe739b9677a05690bc88317f4
Sundaravel M <appmaking@Sundaravels-Mac-mini.local>
2022-12-21 21:48:17 +0530

    08_single_child_scroll

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,66 @@
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
+  runApp(MaterialApp(
+    home: SafeArea(
+      child: Scaffold(
         backgroundColor: Colors.grey[100],
-        body: Center(
-          child: Container(
-            color: Colors.white,
-            height: 400,
-            width: 300,
-            child: Column(
-              children: [
-                Container(
-                  height: 300,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              // Expanded(
+              //   child: Container(
+              //     color: Colors.red,
+              //   ),
+              // ),
+              Text("sdfdsf"),
+              Container(
+                height: 200,
+                child: Container(
+                  height: double.infinity,
                   width: double.infinity,
                   child: Image.asset(
                     "assets/images/logo.png",
                     fit: BoxFit.cover,
                   ),
                 ),
-                Container(
-                  padding: EdgeInsets.all(8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text("Umbber la"),
-                            Text("price"),
-                          ],
-                        ),
+              ),
+              Container(
+                padding: EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Umbber la"),
+                          Text("price"),
+                        ],
                       ),
-                      Container(
-                        child: Row(children: [
-                          Text("4.5"),
-                          Icon(Icons.star),
-                        ]),
-                      ),
-                    ],
-                  ),
+                    ),
+                    Container(
+                      child: Row(children: [
+                        Text("4.5"),
+                        Icon(Icons.star),
+                      ]),
+                    ),
+                  ],
                 ),
-                Container(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      ElevatedButton(onPressed: () {}, child: Text("Buy Now")),
-                      ElevatedButton(
-                          onPressed: () {}, child: Text("Add to Cart"))
-                    ],
-                  ),
+              ),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(onPressed: () {}, child: Text("Buy Now")),
+                    ElevatedButton(onPressed: () {}, child: Text("Add to Cart"))
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
     ),
-  );
+  ));
 }
